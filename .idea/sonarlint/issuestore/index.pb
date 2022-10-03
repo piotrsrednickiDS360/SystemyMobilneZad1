7e2be2ae3ed7463fcac952e6f9c16fc9c5163d9d
pvpetrus <pvpetrus@wp.pl>
2022-10-03 23:33:17 +0200

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+t
+Dapp/src/main/java/com/example/systemymobilnezad1/SecondFragment.java,e\e\eed20915dfe56787727a84d46987c6417ee4d05c
+s
+Capp/src/main/java/com/example/systemymobilnezad1/FirstFragment.java,d\a\da238712ae30126ade07270ceee8478972658130
---
+++ app/src/main/java/com/example/systemymobilnezad1/MainActivity.java
@@ -0,0 +1,130 @@
+package com.example.systemymobilnezad1;
+
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.systemymobilnezad1.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+    private Button trueButton;
+    private Button falseButton;
+    private Button nextButton;
+    private TextView questionTextView;
+    private Question[] questions=new Question[]{
+            new Question(R.string.q_activity,true),
+            new Question(R.string.q_find_resources,true),
+            new Question(R.string.q_listener,true),
+            new Question(R.string.q_resources,true),
+            new Question(R.string.q_version,true)
+    };
+    private int currentIndex;
+    private void checkAnswerCorrectness(boolean userAnswer){
+        boolean correctAnswer=questions[currentIndex].isTrueAnswer();
+        int resultMessageId=0;
+        if (userAnswer==correctAnswer){
+            resultMessageId=R.string.correct_answer;
+        }
+        else{
+            resultMessageId=R.string.incorrect_answer;
+        }
+        Toast.makeText(this,resultMessageId,Toast.LENGTH_SHORT).show();
+    }
+    private void setNextQuestion(){
+        questionTextView.setText(questions[currentIndex].getQuestionId());
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(R.layout.activity_main);
+        trueButton=findViewById(R.id.true_button);
+        falseButton=findViewById(R.id.false_button);
+        nextButton=findViewById(R.id.next_button);
+        questionTextView=findViewById(R.id.question_text_view);
+        //setNextQuestion();
+        trueButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View w){
+                checkAnswerCorrectness(true);
+            }
+        });
+        falseButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View w){
+                checkAnswerCorrectness(false);
+            }
+        });
+        nextButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View w){
+                currentIndex=(currentIndex+1)%questions.length;
+                setNextQuestion();
+            }
+        });
+
+
+
+        setSupportActionBar(binding.toolbar);
+        //NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        //appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
